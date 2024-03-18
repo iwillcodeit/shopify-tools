02aab0037a4f91cf19b4639258ec1d2169cc36ea
Octanium <octaniumcontact@gmail.com>
2024-03-18 22:52:30 +0100

    chore: migrate to SWC
    Signed-off-by: Octanium <octaniumcontact@gmail.com>

--- packages/npm-js/src/executors/tsc/schema.d.ts
+++
@@ -1,3 +0,0 @@
-import type { TypeScriptCompilationOptions } from '@nx/workspace/src/utilities/typescript/compilation';
-
-export interface TscExecutorSchema extends TypeScriptCompilationOptions {} // eslint-disable-line
